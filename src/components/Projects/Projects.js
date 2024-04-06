f7e1ce1f5fc965cd2850e56cc8b1f12aa6b914f3
khanh pham <khanhdanang12345u@example.com>
2024-04-07 00:04:39 +0700

    update project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,10 +5,13 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import portfollio from "../../Assets/Projects/portfolio.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import chatify from "../../Assets/Projects/chatify.png";
+import bookstore from "../../Assets/Projects/bookstore.png";
+import thuongmai from "../../Assets/Projects/thuongmai.png";
+import Cinema from "../../Assets/Projects/Cinema.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -23,45 +26,45 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={portfollio}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Portfollio"
+              description="This is my portfolio page introducing myself with friends build with   "
+              ghLink="https://github.com/phamkhanh1711/Portfolio"
+              demoLink="portfoliokhanh.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={bookstore}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Bookstore"
+              description="My BookStore page build with React.js and React-Bootstrap A book reading website allows users to discover and read a wide variety of books online. Users can explore genres, authors, and titles, access book previews and reviews, and personalize their reading experience. It offers convenient access to a vast digital library, making reading enjoyable and accessible from anywhere."
+              ghLink="https://github.com/phamkhanh1711/Front-end-book"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={thuongmai}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="E-Commerce "
+              description="A clothing booking website lets users browse, select, and buy fashion items online. They can explore clothes, shoes, and accessories from different brands, view product details, add items to cart, and securely checkout. It's a convenient, fast, and diverse online shopping experience.."
+              ghLink="https://github.com/phamkhanh1711/Fe-ban-hang"
+              demoLink="fe-e-commerce.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Cinema}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Cinema Ticket"
+              description="A platform for booking movie tickets online, where users can easily browse current movies, check showtimes, select suitable screenings, and book tickets. The website also provides detailed information about movies, including cast, directors, genres, and ratings. Additional features include user registration for managing personal information and booking history, as well as secure online payments. It offers users a fast, convenient, and enjoyable online ticket booking experience"
+              ghLink="https://github.com/phamkhanh1711/Fe_Cinema"
+             // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
@@ -84,7 +87,7 @@ function Projects() {
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
           </Col>
         </Row>
